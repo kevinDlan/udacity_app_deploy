f898d00262d1581c04cd10294375bc0e4f8d9b79
kevin-nd-kone <kevin.ndkone@gmail.com>
2022-08-14 20:56:42 +0000

    first commit

---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { valideExtension } from './constant/constant';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+
+  app.get("/filteredimage",async (req, res) => {
+    const image_url = req.query.image_url;
+    if(!image_url)
+    {
+      return res.status(403).send({error:"image url is required."})
+    }
+   let urlPart = image_url.split('.');
+   if (!valideExtension.includes(urlPart[urlPart.length - 1]))
+   {
+      return res.status(403).send({error:"provided url is not image url."})
+   }
+    const filterUrl:string = await filterImageFromURL(image_url);
+     res.send(filterUrl);
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
